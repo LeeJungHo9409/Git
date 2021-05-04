d6befb138e981cd172f14f5d43f653ff685ab8d5
LeeJungHo_labtop <dldi121@naver.com>
2021-05-04 16:09:49 +0900

    scof test result

--- nodeEx/NodeEx03.js
+++ nodeEx/NodeEx03.js
@@ -1,13 +1,17 @@
 //자세한 내용은 node.txt를 참고하자.
 
 var a = 0; //a는 함수 스코프 0으로 선언되었다.
+
+/*b,c는 블록 스코프로 선언
 let b = 1;
 const c = 2;
 
+실행해본 결과 scof test console.png 참고
+*/
+
 console.log("함수 밖");
 console.log("var : " + a);
-console.log("let : " + b);
-console.log("const : " + c);
+console.log("let, const 선언안됨 ");
 
 function scof(){
     
@@ -36,8 +40,9 @@ function reviseScof(){
     console.log("const : " + c);
 
     var a = 100;
-    let b = 200;
-    const c = 300;
+    b = 200;
+    //c = 300;
+    //애초에 실행 자체가 안됨.
 
     console.log("Revise Scof 함수 재선언");
     console.log("var : " + a);
@@ -45,3 +50,5 @@ function reviseScof(){
     console.log("const : " + c);
 
 }
+
+reviseScof();
