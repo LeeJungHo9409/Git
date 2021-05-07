4810b6ea3354fe862e7f6a9948dd8a0498d2054a
LeeJungHo_labtop <dldi121@naver.com>
2021-05-07 20:18:54 +0900

    class, structure ++

---
+++ Class/prototype.js
@@ -0,0 +1,38 @@
+
+var Human = function(type){
+    this.type = type || 'human';
+    //this.type은 휴먼 내부 생성된 프로토타입 type
+
+    //console.log(this.type, type);
+};
+
+//Human('bang');
+
+Human.isHuman = function(human){
+    return human instanceof Human;
+}
+
+Human.prototype.breathe = function(){
+    console.log('h-a-a-a-m');
+    //alert('h-a-a-a-m');
+};
+
+var Zero = function(type, firstName, lastName){
+    Human.apply(this,arguments);
+    this.firstName = firstName;
+    this.lastName = lastName;
+
+   //console.log(this.firstName, this.lastName);
+}
+
+//이부분은 이해 어렵..
+Zero.prototype = Object.create(Human.prototype); //new 함수와 같은 기능 프로토타입 객체 생성
+Zero.prototype.constructor = Zero; //상속!
+Zero.prototype.sayName = function(){
+    console.log(this.firstName+''+this.lastName);
+    //alert(this.firstName+''+this.lastName);
+    //줄바꿈 왜 하는지 아직 잘 모르겠음.
+};
+
+var oldZero = new Zero('human', 'Zero', 'Cho');
+Human.isHuman(oldZero);
--- StructureDisassemble/structure.js
+++ StructureDisassemble/structure.js
@@ -1,6 +1,9 @@
 //구조분해 할당
+
+//객체
 console.log("es5");
 
+//공통문법
 var candyMachine = {
     status:{
         name:'node',
@@ -12,9 +15,22 @@ var candyMachine = {
     }
 }
 
+//구문법 es5
 var getCandy = candyMachine.getCandy;
 var count = candyMachine.status.count;
 
 console.log(count);
 
+//신문법 es6+
 const{ getCandy,status:{count}} = candyMachine;
+
+//배열
+var array = ['nodejs', {}, 10, true];
+
+//구문법
+var node = array[0];
+var obj = array[1];
+var bool = array[3];
+
+//신문법 es6+
+const [node2,obj2, bool2] = array;
